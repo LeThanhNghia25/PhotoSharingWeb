cbde87bf47015d938921eed4d277e8d3d81e5b16
ptqani <20130374@st.hcmuaf.edu.vn>
2024-06-07 02:54:31 +0700

    cap nhat lai code

--- src/main/java/models/notificationManager.java
+++
@@ -1,8 +0,0 @@
-package models;
-
-import java.util.ArrayList;
-import java.util.List;
-
-public class notificationManager {
-	private List<notification> notification = new ArrayList<notification>();
-}
