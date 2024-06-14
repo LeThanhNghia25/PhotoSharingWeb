3741c3adabdfd5950878257d9218bbb38381da43
Noah <Noah@Nghia>
2024-06-15 01:08:53 +0700

    sửa đổi csdl, thêm bảng Role, UserRole

--- photosharingweb.sql
+++ photosharingweb.sql
@@ -1,6 +1,8 @@
--- MySQL dump 10.13  Distrib 8.0.34, for Win64 (x86_64)
+CREATE DATABASE  IF NOT EXISTS `photosharingweb` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `photosharingweb`;
+-- MySQL dump 10.13  Distrib 8.0.36, for Win64 (x86_64)
 --
--- Host: localhost    Database: photosharingweb
+-- Host: 127.0.0.1    Database: photosharingweb
 -- ------------------------------------------------------
 -- Server version	8.0.35
 
@@ -41,6 +43,30 @@ CREATE TABLE `admin` (
 LOCK TABLES `admin` WRITE;
 /*!40000 ALTER TABLE `admin` DISABLE KEYS */;
 /*!40000 ALTER TABLE `admin` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `catalog`
+--
+
+DROP TABLE IF EXISTS `catalog`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `catalog` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `catalogname` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `catalog`
+--
+
+LOCK TABLES `catalog` WRITE;
+/*!40000 ALTER TABLE `catalog` DISABLE KEYS */;
+INSERT INTO `catalog` VALUES (1,'Ảnh tĩnh'),(2,'Ảnh động '),(3,'Video');
+/*!40000 ALTER TABLE `catalog` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -102,9 +128,11 @@ CREATE TABLE `img` (
   `content` varchar(250) DEFAULT NULL,
   `img` varchar(350) DEFAULT NULL,
   `createdTime` varchar(50) DEFAULT NULL,
-  `status` varchar(45) DEFAULT NULL,
+  `creator` varchar(100) DEFAULT NULL,
+  `cate` varchar(100) DEFAULT NULL,
+  `enabled` tinyint DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=73 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -113,7 +141,7 @@ CREATE TABLE `img` (
 
 LOCK TABLES `img` WRITE;
 /*!40000 ALTER TABLE `img` DISABLE KEYS */;
-INSERT INTO `img` VALUES (1,'Những Đặc Điểm Dễ Thương Của Mèo Làm Tan Chảy Trái Tim Bạn','Mèo, với vẻ ngoài nhỏ nhắn và những hành động tinh nghịch, luôn khiến con người phải mê mẩn. Đôi mắt tròn to, sáng lấp lánh như hai viên ngọc, cùng với bộ lông mềm mại, mượt mà, tạo nên hình ảnh không thể cưỡng lại. ','resources/img/1.jpg','10-10-2001','ok'),(2,'Mèo Con – Niềm Vui Nhỏ Bé Trong Cuộc Sống','Những chú mèo con, với vẻ ngoài ngây thơ và đôi mắt to tròn, luôn mang đến niềm vui và tiếng cười cho mọi gia đình. Chúng chơi đùa không ngừng, chạy nhảy khắp nơi và khám phá mọi ngóc ngách trong nhà.','resources/img/2.jpg','10-10-2001','ok'),(3,'Đôi mắt to tròn long lanh và chiếc mũi hồng xinh xắn của thỏ con','Thỏ con là loài động vật rất hiền lành và thân thiện. Chúng thích được con người vuốt ve và âu yếm. Hình ảnh thỏ con bú sữa mẹ là biểu tượng của tình mẫu tử thiêng liêng và đầy cảm động.','resources/img/3.jpg','10-10-2001','ok'),(4,'Hình ảnh thỏ con bú sữa mẹ ấm áp và đầy tình cảm','Ngoài ra, thỏ con cũng rất tò mò về thế giới xung quanh. Chúng thường xuyên khám phá mọi ngóc ngách trong nhà và khu vườn. Khi ngủ, thỏ con thường cuộn tròn thành một quả bóng nhỏ trông rất đáng yêu.','resources/img/4.jpg','10-10-2001','ok'),(5,'Bộ sưu tập những khoảnh khắc đáng yêu của thỏ con khi ngủ','hỏ con là một trong những loài động vật nhỏ bé và dễ thương nhất trên thế giới. Chúng sở hữu bộ lông mềm mại như bông, đôi mắt to tròn long lanh và chiếc mũi hồng xinh xắn.','resources/img/5.jpg','10-10-2001','ok'),(6,'Thỏ con và những người bạn động vật khác trong trang trại','Nuôi thỏ con không chỉ mang đến niềm vui mà còn giúp chúng ta giải tỏa căng thẳng và giảm bớt stress. Tiếng kêu líu lo của thỏ con như một bản nhạc du dương giúp ta thư giãn tâm hồn.','resources/img/6.jpg','10-10-2001','ok'),(7,'Hành trình khám phá thế giới xung quanh của thỏ con','Với vẻ ngoài dễ thương và tính cách hiền lành, thỏ con đã trở thành một trong những loài thú cưng được yêu thích nhất trên thế giới. Chúng là món quà tuyệt vời dành cho những ai yêu động vật và muốn có một người bạn đồng hành đáng yêu.','resources/img/7.jpg','10-10-2001','ok'),(8,'Chú thỏ con mang đến niềm vui và tiếng cười cho mọi người','Hình ảnh thỏ con tinh nghịch nhảy nhót và nô đùa trong vườn cỏ mang đến niềm vui và sự thư giãn.','resources/img/8.jpg','10-10-2001','ok'),(9,'Thỏ con là một trong những loài động vật nhỏ bé và dễ thương','Thỏ con là loài hiền lành, thân thiện, thích được vuốt ve và âu yếm.','resources/img/9.jpg','10-10-2001','ok');
+INSERT INTO `img` VALUES (7,'Hành trình khám phá thế giới xung quanh của thỏ con','Với vẻ ngoài dễ thương và tính cách hiền lành, thỏ con đã trở thành một trong những loài thú cưng được yêu thích nhất trên thế giới. Chúng là món quà tuyệt vời dành cho những ai yêu động vật và muốn có một người bạn đồng hành đáng yêu.','resources/img/7.jpg','10-10-2001','1','2',1),(8,'Chú thỏ con mang đến niềm vui và tiếng cười cho mọi người','Hình ảnh thỏ con tinh nghịch nhảy nhót và nô đùa trong vườn cỏ mang đến niềm vui và sự thư giãn.','resources/img/8.jpg','10-10-2001','1','2',1);
 /*!40000 ALTER TABLE `img` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -141,6 +169,30 @@ LOCK TABLES `notification` WRITE;
 UNLOCK TABLES;
 
 --
+-- Table structure for table `role`
+--
+
+DROP TABLE IF EXISTS `role`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `role` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `role`
+--
+
+LOCK TABLES `role` WRITE;
+/*!40000 ALTER TABLE `role` DISABLE KEYS */;
+INSERT INTO `role` VALUES (1,'ROLE_ADMIN'),(2,'ROLE_USER');
+/*!40000 ALTER TABLE `role` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `user`
 --
 
@@ -148,14 +200,15 @@ DROP TABLE IF EXISTS `user`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `user` (
-  `id` int NOT NULL,
+  `id` int NOT NULL AUTO_INCREMENT,
   `username` varchar(100) DEFAULT NULL,
   `password` varchar(200) DEFAULT NULL,
+  `describe` varchar(600) DEFAULT NULL,
   `email` varchar(150) DEFAULT NULL,
   `birthday` date DEFAULT NULL,
-  `status` varchar(45) DEFAULT NULL,
+  `enabled` tinyint DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -164,17 +217,38 @@ CREATE TABLE `user` (
 
 LOCK TABLES `user` WRITE;
 /*!40000 ALTER TABLE `user` DISABLE KEYS */;
-INSERT INTO `user` VALUES (1,'quan','123','quanquan174@yahoo.com','1985-08-15','active');
+INSERT INTO `user` VALUES (1,'Anh Quaan','$2a$10$i3Q7wFNDc357iDa6rJyN7.jrQ3okAJa4lIUBmZ8.vYZlg3xungsUG',NULL,'quanquan174@yahoo.com','1985-08-15',1), (2,'Nghia','$2a$10$i3Q7wFNDc357iDa6rJyN7.jrQ3okAJa4lIUBmZ8.vYZlg3xungsUG',NULL,'lethanhnghia0938@gmail.com','2002-05-02',1),(3,'quan@gmail.com','$2a$10$i3Q7wFNDc357iDa6rJyN7.jrQ3okAJa4lIUBmZ8.vYZlg3xungsUG',NULL,'sample@example.com','2111-12-31',1),(4,'leb','$2a$10$i3Q7wFNDc357iDa6rJyN7.jrQ3okAJa4lIUBmZ8.vYZlg3xungsUG',NULL,'leb@gmail.com','2024-05-30',1);
 /*!40000 ALTER TABLE `user` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
--- Dumping events for database 'photosharingweb'
+-- Table structure for table `users_roles`
 --
 
+DROP TABLE IF EXISTS `users_roles`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `users_roles` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `userId` int NOT NULL,
+  `roleId` int NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `userId` (`userId`),
+  KEY `roleId` (`roleId`),
+  CONSTRAINT `users_roles_ibfk_1` FOREIGN KEY (`userId`) REFERENCES `user` (`id`),
+  CONSTRAINT `users_roles_ibfk_2` FOREIGN KEY (`roleId`) REFERENCES `role` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
 --
--- Dumping routines for database 'photosharingweb'
+-- Dumping data for table `users_roles`
 --
+
+LOCK TABLES `users_roles` WRITE;
+/*!40000 ALTER TABLE `users_roles` DISABLE KEYS */;
+INSERT INTO `users_roles` VALUES (1,1,1),(2,2,1),(3,3,2),(4,4,2);
+/*!40000 ALTER TABLE `users_roles` ENABLE KEYS */;
+UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -185,4 +259,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-05-25  0:39:09
+-- Dump completed on 2024-06-15  1:03:02
